7cde89858fc60850d0ed29e625971f225961d894
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-02-12 17:14:13 +0100

    Code refactoring and fix issue with new channel names not being fetched when navigating between youtube.com and youtube.com/feed/subscriptions.

--- addMissingChannelNames.js
+++ addMissingChannelNames.js
@@ -1,56 +1,66 @@
-setTimeout(attemptToAddChannelNamesToVideos, 100);
+setInterval(attemptToAddChannelNamesToVideos, 100);
 
+let shorts = {
+    url: document.querySelectorAll('a[href^="/shorts/"]'),
+    requestedChannelName: [],
 
-function attemptToAddChannelNamesToVideos() {
-    if (shortLinksAreAvailable()) {
-        addUploaderNameToShortsOnMainPage();
-    } else {
-        setTimeout(attemptToAddChannelNamesToVideos, 100);
-    }
-}
+    exist() {
+        return document.querySelectorAll('a[href^="/shorts/"]').length;
+    },
 
-function addUploaderNameToShortsOnMainPage() {
+    getUrls() {
+        this.url = document.querySelectorAll('a[href^="/shorts/"]');
+    },
+
+    hasTitle(url) {
+        return url.querySelector('#video-title');
+    },
     
-    const shortsLinks = document.querySelectorAll('a[href^="/shorts/"]');
+    isMissingChannelName(url) {
+        return !url.querySelector('#channel-name') && !this.requestedChannelName.includes(url);
+    },
 
-    for (let i = 0; i < shortsLinks.length; i++) {
-        if (hasTitle(shortsLinks[i])){
-            getChannelNameFromShortsLink(shortsLinks[i])
-            .then(channelName => {
-                appendChannelNameToDOM(shortsLinks[i], channelName);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            })
-        }
-    }
+    dontFetchForThisUrlAgain(url) {
+        this.requestedChannelName.push(url);
+    },
 
-    function appendChannelNameToDOM(link, channelName) {
+    appendChannelName(link, channelName) {
         const titleElement = link.querySelector('#video-title');
         const div = document.createElement('div');
         div.innerHTML = '<a id="channel-name" title="' + channelName + '"; style="color: #AAAAAA; font: 14px Roboto, Arial, sans-serif; margin: 0 -1.4px 0 0; padding: 0 -1.4px 0 0; text-decoration: none;">' + channelName + '</a>';
         titleElement.parentNode.appendChild(div);
-    }
+    },
 
-    function hasTitle(link) {
-        return link.querySelector('#video-title')
-    }
+    async fetchChannelName(url) {
+        const response = await fetch(url);
+        const html = await response.text();
+        const channelNameRegex = /"ownerChannelName":"([^"]+)"/;
+        const match = html.match(channelNameRegex);
+        if (match && match[1]) {
+            const channelName = match[1];
+            return channelName;
+        } else {
+            return null;
+        }
+    },
+    
 }
 
-async function getChannelNameFromShortsLink(link) {
-    const response = await fetch(link);
-    const html = await response.text();
-    console.log(html);
-    const channelNameRegex = /"ownerChannelName":"([^"]+)"/;
-    const match = html.match(channelNameRegex);
-    if (match && match[1]) {
-        const channelName = match[1];
-        return channelName;
-    } else {
-        return null;
+function attemptToAddChannelNamesToVideos() {
+    if (shorts.exist()) {
+        shorts.getUrls();
+        for (let i = 0; i < shorts.url.length; i++) {
+            const url = shorts.url[i];
+            if (shorts.hasTitle(url) && shorts.isMissingChannelName(url)) {
+                shorts.dontFetchForThisUrlAgain(url);
+                shorts.fetchChannelName(url)
+                .then(channelName => {
+                    shorts.appendChannelName(url, channelName);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+            }
+        }
     }
-}
-
-function shortLinksAreAvailable() {
-    return document.querySelectorAll('a[href^="/shorts/"]').length;
 }
